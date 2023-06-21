396237f5b70cf53b17993eada3d5c54168065c7e
neothenewone <124289169+neothenewone@users.noreply.github.com>
2023-06-21 15:42:16 -0300

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 import { Sequelize,Model, DataTypes } from "sequelize";
 
-const db=new Sequelize('isepsantafe_ingreso2024','isepsantafe_ingreso2024','Iamneo2112@',{
+const db=new Sequelize('apps_ingreso2024','apps_ingreso2024','Iamneo2112@',{
     host:'127.0.0.1',
     dialect:'mysql'
 });
